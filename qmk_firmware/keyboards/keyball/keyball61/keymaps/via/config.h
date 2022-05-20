7b3cbfc9f32638883d608e7316468f2a9cd8c07a
yoshida-m-3 <48209986+yoshida-m-3@users.noreply.github.com>
2022-05-20 23:22:14 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -18,7 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define TAPPING_TERM 100
+#define TAPPING_TERM 0
+#define PERMISSVE_HOLD
+#define RETRO_TAPPING
 
 #pragma once
 
