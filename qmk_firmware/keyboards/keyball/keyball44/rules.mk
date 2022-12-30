236df63aba8249bd3f84016b6182093bc80ba9fc
yoshida-m-3 <48209986+yoshida-m-3@users.noreply.github.com>
2022-12-30 17:56:31 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball44/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/rules.mk
@@ -13,7 +13,7 @@ EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 
 # Keyball39 is split keyboard.
