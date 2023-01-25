147c14c5ca62a26cc691795f8d433f914f76ab0f
Makoto Yoshida <milmed1013@gmail.com>
2023-01-25 09:42:33 +0900

    update keymap

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -24,30 +24,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
-    LCTL_T(KC_ESC)   , KC_A     , KC_S     , KC_D     , LT(3, KC_F)     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    GUI_T(KC_TAB)   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
+    LCTL_T(KC_ESC)   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BTN1  ,
-              KC_LALT,LSG(KC_5),  MO(2),LT(1,KC_SPC),MO(3),                  KC_BSPC,GUI_T(KC_ENT), RCTL_T(KC_LANG2),     _______  , KC_BTN2
+              KC_LALT,LSG(KC_5),  MO(2),LT(1,KC_SPC),MO(3),                  KC_BACKSPACE,GUI_T(KC_ENT), RCTL_T(KC_LANG2),     _______  , TG(1)
   ),
 
   [1] = LAYOUT_universal(
     KC_GRV  ,  KC_EXLM   , KC_AT    , KC_HASH   , KC_DOLLAR    , KC_PERCENT    ,                             KC_CIRCUMFLEX    , KC_AMPERSAND    , KC_ASTERISK    , KC_LEFT_PAREN    , KC_RIGHT_PAREN   , KC_EQL   ,
-    _______  ,  _______ , KC_BTN2  , KC_BTN3   , KC_BTN1   , _______   ,                                     KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT  , KC_BTN3  , KC_F12   ,
+    _______  ,  KC_MINS , KC_BTN2  , KC_BTN3   , KC_BTN1   , KC_EQL   ,                                     KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT  , KC_LBRC  , KC_RBRC  ,
     _______  ,  _______ , _______  , _______ , _______  , _______  ,                                         _______  , KC_LNG2  , KC_LNG1  , _______  , KC_BACKSLASH  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , KC_ESC  , _______       , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
-    KC_TILD  ,_______, _______     , _______    , _______     , _______  ,                                         _______  , KC_7  , KC_8  , KC_9  , _______  , KC_PLUS  ,
-    _______  ,_______, _______     , _______    , _______     , _______  ,                                         _______  , KC_4  , KC_5, KC_6  , KC_LCBR  , KC_RCBR  ,
-    _______  ,_______, _______     , _______    , _______     ,_______,                                        KC_DOT,KC_1, KC_2   ,KC_3,KC_PIPE,_______,
-                  _______, _______, _______  ,         _______  , _______  ,                   _______   , KC_0, _______       , _______  , _______
+    KC_TILD  ,_______, _______     , _______    , _______     , _______  ,                                         _______  , KC_KP_7  , KC_KP_8  , KC_KP_9  , _______  , KC_PLUS  ,
+    _______  ,_______, _______     , _______    , _______     , _______  ,                                         _______  , KC_KP_4  , KC_KP_5, KC_KP_6  , KC_LCBR  , KC_RCBR   ,
+    _______  ,_______, _______     , _______    , _______     ,_______,                                        KC_KP_DOT,KC_KP_1, KC_KP_2   ,KC_KP_3,KC_PIPE,_______,
+                  _______, _______, _______  ,         _______  , _______  ,                   _______   , KC_KP_0, _______       , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
     RGB_TOG  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                        KC_F6  , KC_F7  , KC_F8  , KC_F9 , KC_F10 , KC_F11  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , CPI_I100  , SCRL_DVI ,                                        LCA(KC_LEFT)  , _______  , LCA(KC_ENT)  , LCA(KC_RIGHT) , KC_KB_VOLUME_UP  , KC_F12  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , CPI_D100  , SCRL_DVD ,                                        _______  , _______ , _______ , _______  , KC_KB_VOLUME_DOWN  , _______ ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , CPI_I100  , SCRL_DVI ,                                        LCA(KC_LEFT)  , LCA(KC_F)  , LCA(KC_ENT)  , LCA(KC_RIGHT) , KC_KB_VOLUME_UP  , KC_F12  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , CPI_D100  , SCRL_DVD ,                                        LCA(KC_D)  , LCA(KC_E) , LCA(KC_T) , LCA(KC_G)  , KC_KB_VOLUME_DOWN  , _______ ,
                   _______    , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , RESET
   ),
 };
--- qmk_firmware/keyboards/keyball/keyball44/readme.md
+++ qmk_firmware/keyboards/keyball/keyball44/readme.md
@@ -12,6 +12,7 @@ A split keyboard with 44 vertically staggered keys and 34mm track ball.
 Make example for this keyboard (after setting up your build environment):
 
     make keyball/keyball44:default
+    $ qmk compile -kb keyball/keyball44 -km default
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
