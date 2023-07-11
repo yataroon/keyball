ecb46fb64978be86fa24914c0b8e486c41a08343
yataroon <108793413+yataroon@users.noreply.github.com>
2023-07-12 06:54:55 +0900

    2023-07-12-04

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 #define HOLD_ON_OTHER_KEY_PRESS
-#define TAPPING_TERM 100
+// #define TAPPING_TERM 100
 
 // USB Device descriptor parameters
 // #define VENDOR_ID           0x5957      // "YW" = Yowkees
